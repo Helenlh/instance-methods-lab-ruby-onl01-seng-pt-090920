7894445e2dad1ceea9ccf650d9341654526bee34
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-04 01:08:05 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person 
+  def talk
+    puts "Hello World!"
+  end 
+end 
+
+ada = Person.new 
+ada.talk 
+puts "Hello World!"
+
+class Person 
+  def walk
+    puts "The Person is walking"
+  end 
+end 
+
+ada = Person.new 
+ada.walk 
+puts "The Perso is walking" 
+

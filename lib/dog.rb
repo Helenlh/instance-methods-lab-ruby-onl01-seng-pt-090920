cffed37fc080d87eda6cc0e5dfed892ad44210ce
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-04 01:19:42 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def bark
+  puts "Woof!"
+end 
+end 
+
+  fido = Dog.new 
+  fido.bark 
+  puts "Woof!"
+
+
+class Dog
+  def sit 
+    puts "The Dog is sitting"
+  end 
+end 
+
+fido = Dog.new 
+fido.sit 
+puts "The Dog is sitting"
